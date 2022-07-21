b9f8268d3ba987a941f90c52dff4c2d4105aeaa1
MauroKinderknecht <mauro.kinderknecht@ing.austral.edu.ar>
2022-07-21 15:24:40 +0000

    ssl config

--- init.sh
+++ init.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v docker-compose)" ]; then
-  echo 'Error: docker-compose is not installed.' >&2
-  exit 1
-fi
-
 domains=(jibberjabber.cloud www.jibberjabber.cloud)
 rsa_key_size=4096
 data_path="./certbot"
 email="mauro.kinderknecht@ing.austral.edu.ar" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -30,7 +25,7 @@ fi
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
-docker-compose run --rm --entrypoint "\
+docker compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:1024 -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
@@ -39,11 +34,11 @@ echo
 
 
 echo "### Starting nginx ..."
-docker-compose up --force-recreate -d proxy
+docker compose up --force-recreate -d proxy
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
-docker-compose run --rm --entrypoint "\
+docker compose run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/live/$domains && \
   rm -Rf /etc/letsencrypt/archive/$domains && \
   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
@@ -66,7 +61,7 @@ esac
 # Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-docker-compose run --rm --entrypoint "\
+docker compose run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
@@ -77,7 +72,7 @@ docker-compose run --rm --entrypoint "\
 echo
 
 echo "### Reloading nginx ..."
-docker-compose exec proxy proxy -s reload
+docker compose exec proxy proxy -s reload
 
 echo "### Docker Compose up"
-docker-compose up -d 
+docker compose up -d 
